896b5410b09a1f16ecd0d76dcb5575a17802024a
song <minns2992@gmail.com>
2022-11-02 15:20:41 +0900

    create dev branch

--- app/src/main/java/com/song/githubtest/MainActivity.kt
+++ app/src/main/java/com/song/githubtest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.song.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val tv:TextView=findViewById(R.id.tv)
+        tv.setOnClickListener { Toast.makeText(this, "clicked button", Toast.LENGTH_SHORT).show() }
     }
 }
